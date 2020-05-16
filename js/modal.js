ab31e547eaeb57d992ac9312988ea797ecb67077
Nisha Chatwani <nchatwan@usc.edu>
2020-05-15 19:01:12 -0700

    spring 2020

--- js/modal.js
+++ js/modal.js
@@ -1,24 +1,36 @@
 // Get the modal
-var modal1 = document.getElementById('modal-prosum');
-var modal2 = document.getElementById('modal-boeing');
-var modal3 = document.getElementById('modal-lab');
-var modal4 = document.getElementById('modal-cais');
-var modal5 = document.getElementById('modal-scope');
-var modal6 = document.getElementById('modal-soccer');
+var modal1 = document.getElementById('modal-raytheon');
+var modal2 = document.getElementById('modal-prosum');
+var modal3 = document.getElementById('modal-boeing');
+var modal4 = document.getElementById('modal-lab');
+var modal5 = document.getElementById('modal-cais');
+var modal6 = document.getElementById('modal-wic');
+var modal7 = document.getElementById('modal-hh');
+var modal8 = document.getElementById('modal-df');
+var modal9 = document.getElementById('modal-swe');
+var modal10 = document.getElementById('modal-soccer');
 
 // Get the button that opens the modal
-var btn1 = document.getElementById("img-prosum");
+var btn1 = document.getElementById("img-ray");
 var btn1a = document.getElementsByClassName("exp")[0];
-var btn2 = document.getElementById("img-boeing");
+var btn2 = document.getElementById("img-prosum");
 var btn2a = document.getElementsByClassName("exp")[1];
-var btn3 = document.getElementById('img-lab');
+var btn3 = document.getElementById('img-boeing');
 var btn3a = document.getElementsByClassName("exp")[2];
-var btn4 = document.getElementById('img-cais');
+var btn4 = document.getElementById('img-lab');
 var btn4a = document.getElementsByClassName("exp")[3];
-var btn5 = document.getElementById('img-scope');
+var btn5 = document.getElementById('img-cais');
 var btn5a = document.getElementsByClassName("exp")[4];
-var btn6 = document.getElementById('img-soccer');
+var btn6 = document.getElementById('img-wic');
 var btn6a = document.getElementsByClassName("exp")[5];
+var btn7 = document.getElementById('img-hh');
+var btn7a = document.getElementsByClassName("exp")[6];
+var btn8 = document.getElementById('img-df');
+var btn8a = document.getElementsByClassName("exp")[6];
+var btn9 = document.getElementById('img-swe');
+var btn9a = document.getElementsByClassName("exp")[6];
+var btn10 = document.getElementById('img-soccer');
+var btn10a = document.getElementsByClassName("exp")[6];
 
 //Get the button that closes the modal
 var closebtn1 = document.getElementById("close-button1");
@@ -27,6 +39,10 @@ var closebtn3 = document.getElementById("close-button3");
 var closebtn4 = document.getElementById("close-button4");
 var closebtn5 = document.getElementById("close-button5");
 var closebtn6 = document.getElementById("close-button6");
+var closebtn7 = document.getElementById("close-button7");
+var closebtn8 = document.getElementById("close-button8");
+var closebtn9 = document.getElementById("close-button9");
+var closebtn10 = document.getElementById("close-button10");
 
 // Get the <span> element that closes the modal
 var span1 = document.getElementById("close1");
@@ -35,6 +51,10 @@ var span3 = document.getElementById("close3");
 var span4 = document.getElementById("close4");
 var span5 = document.getElementById("close5");
 var span6 = document.getElementById("close6");
+var span7 = document.getElementById("close7");
+var span8 = document.getElementById("close8");
+var span9 = document.getElementById("close9");
+var span10 = document.getElementById("close10");
 
 // When the user clicks the button, open the modal 
 btn1.onclick = function() {
@@ -73,6 +93,32 @@ btn6.onclick = function() {
 btn6a.onclick = function() {
   modal6.style.display = "block";
 }
+btn7.onclick = function() {
+  modal7.style.display = "block";
+}
+btn7a.onclick = function() {
+  modal7.style.display = "block";
+}
+btn8.onclick = function() {
+  modal8.style.display = "block";
+}
+btn8a.onclick = function() {
+  modal8.style.display = "block";
+}
+btn9.onclick = function() {
+  modal9.style.display = "block";
+}
+btn9a.onclick = function() {
+  modal9.style.display = "block";
+}
+btn10.onclick = function() {
+  modal10.style.display = "block";
+}
+btn10a.onclick = function() {
+  modal10.style.display = "block";
+}
+
+
 
 // When the user clicks on <span> (x), close the modal
 span1.onclick = function() {
@@ -93,6 +139,18 @@ span5.onclick = function() {
 span6.onclick = function() {
   modal6.style.display = "none";
 }
+span7.onclick = function() {
+  modal7.style.display = "none";
+}
+span8.onclick = function() {
+  modal8.style.display = "none";
+}
+span9.onclick = function() {
+  modal9.style.display = "none";
+}
+span10.onclick = function() {
+  modal10.style.display = "none";
+}
 
 //Wnen the user clicks on the close button, close the modal
 closebtn1.onclick = function() {
@@ -112,6 +170,18 @@ closebtn5.onclick = function() {
 }
 closebtn6.onclick = function() {
   modal6.style.display = "none";
+}
+closebtn7.onclick = function() {
+  modal7.style.display = "none";
+}
+closebtn8.onclick = function() {
+  modal8.style.display = "none";
+}
+closebtn9.onclick = function() {
+  modal9.style.display = "none";
+}
+closebtn10.onclick = function() {
+  modal10.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
